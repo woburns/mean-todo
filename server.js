d9035a54b2d30b405770a8824d6bfb0514594eac
Wayne Burns <woburns@gmail.com>
2017-07-11 17:31:18 -0400

    Update to mongo

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ app.get("*", function(req, res){
     res.sendfile("./public/index2.html");
 });
 // listen (start app with node server.js) ======================================
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
