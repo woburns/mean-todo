2aa38071a05ad78b43ef1bc9217c2de78f3a2339
Wayne Burns <woburns@gmail.com>
2017-07-11 17:51:46 -0400

    Update to mongo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 // configuration =================
 
 console.log("starting");
-var db = mongoose.connect('mongodb://mean-todo:password1@ds153652.mlab.com:53652/woburns-todo',{useMongoClient: true});     // connect to mongoDB database on modulus.io
+var db = mongoose.connect('MONGODB_URI',{useMongoClient: true});     // connect to mongoDB database on modulus.io
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
