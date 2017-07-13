298a3462e223bd24dd738753a89783450233299c
Wayne Burns <woburns@gmail.com>
2017-07-13 12:26:10 -0400

    added cors header handling

--- server.js
+++ server.js
@@ -18,7 +18,25 @@ app.use(bodyParser.urlencoded({'extended':'true'}));            // parse applica
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // define model
 var Todo = mongoose.model('Todo', {
@@ -33,6 +51,7 @@ app.get("/api/todos", function(req, res){
         {
             res.send(err);
         }
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
         res.json(todos);
     });
 });
